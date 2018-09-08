f01e876c58cacf8b9edbbbaa128be81f6d11511d
Collin Yablon <collinyablon@gmail.com>
2018-09-08 15:49:33 -0600

    install es-lint and cleanup formatting errors

--- api/github-search.js
+++ api/github-search.js
@@ -3,25 +3,25 @@
 const Joi = require('joi')
 
 module.exports = {
-    name: 'github-search',
-    version: '0.0.1',
-    register: async function (server) {
+  name: 'github-search',
+  version: '0.0.1',
+  register: async function (server) {
 
-        server.route({
-            method:'GET',
-            path:'/repositories/search',
-            options: {
-                validate: {
-                    query: {
-                        q: Joi.string().required(),
-                        sort: Joi.string().allow(['stars', 'forks', 'updated'])
-                    }
-                }
-            },
-            handler: async function(request) {
+    server.route({
+      method:'GET',
+      path:'/repositories/search',
+      options: {
+        validate: {
+          query: {
+            q: Joi.string().required(),
+            sort: Joi.string().allow(['stars', 'forks', 'updated'])
+          }
+        }
+      },
+      handler: async function(request) {
         
-                return []
-            }
-        })
-    }
+        return request.query.sort || []
+      }
+    })
+  }
 }
--- api/index.js
+++ api/index.js
@@ -3,28 +3,30 @@
 const Hapi = require('hapi')
 
 const server = Hapi.server({
-    host: 'localhost',
-    port: 8000
+  host: 'localhost',
+  port: 8000
 })
 
 async function start() {
 
-    await server.register({
-        plugin: require('./github-search'),
-        options: {
-            message: 'hello'
-        }
-    })
-
-    try {
-        await server.start()
+  await server.register({
+    plugin: require('./github-search'),
+    options: {
+      message: 'hello'
     }
-    catch (err) {
-        console.log(err)
-        process.exit(1)
-    }
+  })
 
-    console.log('Server running at:', server.info.uri)
+  /* eslint-disable no-console */
+  try {
+    await server.start()
+  }
+  catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
+
+  console.log('Server running at:', server.info.uri)
+  /* eslint-enable no-console */
 }
 
 start()
